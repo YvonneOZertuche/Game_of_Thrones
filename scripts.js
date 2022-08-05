7ef84c45fa0c5383978087d0e07ce3d47146f016
YvonneOZertuche <aymzertuche1@outlook.com>
2022-08-05 17:36:02 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ let allHouse = []
 const gotMain = async() => {
   let htmlFragment = ""
 
-
+//Fetching the houses and storing in variable allHouse
   for (let i = 1; i <= 9; i++){
-    let fetchHouse = await fetch(`https://www.anapioficeandfire.com/api/houses?pages=${i}&pageSize=50`)
+    let fetchHouse = await fetch(`https://www.anapioficeandfire.com/api/houses?page=${i}&pageSize=50`)
     let housesList = await fetchHouse.json()
     let objHouse = housesList
     objHouse.forEach(element => {
@@ -14,32 +14,52 @@ const gotMain = async() => {
     });
   }
  
+ //Fetching character names and storing in variable nameChar
  for (let i = 1; i <= 43; i++){
-  let fetchName = await fetch(`https://www.anapioficeandfire.com/api/characters?pages=${i}&pageSize=50`)
+  let fetchName = await fetch(`https://www.anapioficeandfire.com/api/characters?page=${i}&pageSize=50`)
   let namesList = await fetchName.json()
   let objName = namesList
   objName.forEach(element => {
     nameChar.push(element)
   })
-
  }
-// console.log(allHouse)
-// console.log(nameChar)
 
-nameChar.forEach(element => {
-  console.log(element.name) 
-  htmlFragment += `<li>${element.name}</li>`
-  
-})
 
+//creating an element
 let ul = document.querySelector('ul')
-ul.innerHTML = htmlFragment
+const createElementLi = (charName, nameOfHouse) => {
+  htmlFragment += `
+  <li>${charName}
+  of ${nameOfHouse}.</li>
+  `
+  ul.innerHTML = htmlFragment
+} 
 
-
-
-
-
+nameChar.forEach(object => {
+  if (object.allegiances.length == 0){
+    // console.log(`Sorry, ${object.name} does not have a house.`)
+} else if (object.allegiances.length !=0){
+    let houseNumber = object.allegiances[0].slice(45)
+    let houseName = allHouse[houseNumber - 1].name
+    // console.log(`${object.name}'s house is ${houseName}.`)
+    createElementLi(object.name, houseName) 
 }
-// gotMain();
+
+
+})
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+gotMain()
 
 
