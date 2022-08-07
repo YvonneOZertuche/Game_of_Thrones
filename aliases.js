fd1f6f6d7fe617f5656395b5e8831d9809c92430
YvonneOZertuche <aymzertuche1@outlook.com>
2022-08-06 20:42:31 -0500

    changes

---
+++ aliases.js
@@ -0,0 +1,72 @@
+let nameChar = []
+let allHouse = []
+
+const gotMain = async () => {
+  let htmlFragment = ''
+
+  //fetch for House
+  for (let i = 1; i <= 9; i++) {
+    let fetchHouse = await fetch(
+      `https://www.anapioficeandfire.com/api/houses?page=${i}&pageSize=50`
+    )
+    let housesList = await fetchHouse.json()
+    let objHouse = housesList
+    objHouse.forEach(element => {
+      allHouse.push(element)
+    })
+  }
+
+  //fetch for character Name
+  for (let i = 1; i <= 43; i++) {
+    let fetchName = await fetch(
+      `https://www.anapioficeandfire.com/api/characters?page=${i}&pageSize=50`
+    )
+    let namesList = await fetchName.json()
+    let objName = namesList
+    objName.forEach(element => {
+      nameChar.push(element)
+    })
+  }
+
+  //Create HTML element 'li' within a ul
+  let ul = document.querySelector('ul')
+  const createElementLi = (charName, nameOfHouse) => {
+    htmlFragment += `
+        <li>${charName}
+        of ${nameOfHouse}.</li>
+        `
+    ul.innerHTML = htmlFragment
+  }
+ 
+
+  nameChar.forEach(object => {
+    if (object.allegiances.length == 0) {
+      // console.log(`Sorry, ${object.name} does not have a house.`)
+    } else if (object.allegiances.length != 0) {
+      let houseNumber = object.allegiances[0].slice(45)
+      let houseName = allHouse[houseNumber - 1].name
+      // console.log(`${object.name}'s house is ${houseName}.`)
+      createElementLi(object.name, houseName)
+    }
+  })
+
+  let aliases = nameChar
+  for (let i = 0; i <= nameChar.length; i++) {  
+    console.log(`${aliases[i].aliases}`)
+ 
+  }
+
+console.log(nameChar)
+console.log(allHouse)
+
+
+
+
+
+
+
+
+
+}
+
+gotMain()
--- scripts.js
+++ scripts.js
@@ -1,65 +1,66 @@
 let nameChar = []
 let allHouse = []
 
-const gotMain = async() => {
-  let htmlFragment = ""
+const gotMain = async () => {
+  let htmlFragment = ''
 
-//Fetching the houses and storing in variable allHouse
-  for (let i = 1; i <= 9; i++){
-    let fetchHouse = await fetch(`https://www.anapioficeandfire.com/api/houses?page=${i}&pageSize=50`)
-    let housesList = await fetchHouse.json()
-    let objHouse = housesList
-    objHouse.forEach(element => {
-      allHouse.push(element)      
-    });
-  }
- 
- //Fetching character names and storing in variable nameChar
- for (let i = 1; i <= 43; i++){
-  let fetchName = await fetch(`https://www.anapioficeandfire.com/api/characters?page=${i}&pageSize=50`)
-  let namesList = await fetchName.json()
-  let objName = namesList
-  objName.forEach(element => {
-    nameChar.push(element)
-  })
- }
+  //Houses
+    for (let i = 1; i <= 9; i++) {
+      let fetchHouse = await fetch(
+        `https://www.anapioficeandfire.com/api/houses?page=${i}&pageSize=50`
+      )
+      let housesList = await fetchHouse.json()
+      let objHouse = housesList
+      objHouse.forEach(element => {
+        allHouse.push(element)
+      })
+    }
 
+  //Character Names
+    for (let i = 1; i <= 43; i++) {
+      let fetchName = await fetch(
+        `https://www.anapioficeandfire.com/api/characters?page=${i}&pageSize=50`
+      )
+      let namesList = await fetchName.json()
+      let objName = namesList
+      objName.forEach(element => {
+        nameChar.push(element)
+      })
+    }
 
-//creating an element
-let ul = document.querySelector('ul')
-const createElementLi = (charName, nameOfHouse) => {
-  htmlFragment += `
-  <li>${charName}
-  of ${nameOfHouse}.</li>
-  `
-  ul.innerHTML = htmlFragment
-} 
+    //creating an element
+      let ul = document.querySelector('ul')
+      const createElementLi = (charName, nameOfHouse) => {
+        htmlFragment += `
+        <li>${charName}
+        of ${nameOfHouse}.</li>
+        `
+        ul.innerHTML = htmlFragment
+      }
+    // console.log(nameChar)
+    // console.log(allHouse)
 
-nameChar.forEach(object => {
-  if (object.allegiances.length == 0){
-    // console.log(`Sorry, ${object.name} does not have a house.`)
-} else if (object.allegiances.length !=0){
-    let houseNumber = object.allegiances[0].slice(45)
-    let houseName = allHouse[houseNumber - 1].name
-    // console.log(`${object.name}'s house is ${houseName}.`)
-    createElementLi(object.name, houseName) 
+    nameChar.forEach(object => {
+      if (object.allegiances.length == 0) {
+        // console.log(`Sorry, ${object.name} does not have a house.`)
+      } else if (object.allegiances.length != 0) {
+        let houseNumber = object.allegiances[0].slice(45)
+        let houseName = allHouse[houseNumber - 1].name
+        // console.log(`${object.name}'s house is ${houseName}.`)
+        createElementLi(object.name, houseName)
+      }
+    })
+
+    let aliases = nameChar
+    for (let i = 0; i <= nameChar.length; i++){
+    console.log(`${aliases[i].aliases}`)
 }
 
-
-})
-  
-}
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    }
 
 gotMain()
-
 
